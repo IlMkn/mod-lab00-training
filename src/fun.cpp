ead0f14d34720ccf93cc48d008c19aa79ea5dc30
IlMkn <91212388+IlMkn@users.noreply.github.com>
2022-03-04 15:35:43 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -3,16 +3,5 @@
 
 int64_t power(int64_t x, uint16_t n) 
 {
-    int result = 0;
-    if (n == 0)
-        result = 1;
-    else
-    {
-        result = x;
-        for (int i = 0; i < n; i++)
-        {
-            result = result * x;
-        }
-    }
-    return result;
+    return 0;
 }
