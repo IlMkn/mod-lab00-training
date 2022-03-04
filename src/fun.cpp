85879b2837a3b22d2a5f535f3756bac7dd7c078f
kekus <bobbiethekot@gmail.com>
2022-03-04 15:29:35 +0300

    new commit

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,6 +1,18 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
 
-int64_t power(int64_t x, uint16_t n) {
-    return 0;
+int64_t power(int64_t x, uint16_t n) 
+{
+    int result = 0;
+    if (n == 0)
+        result = 1;
+    else
+    {
+        result = x;
+        for (int i = 0; i < n; i++)
+        {
+            result = result * x;
+        }
+    }
+    return result;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
+#include <iostream>
+using namespace std;
 
-int main() {
-    return 0;
+int main() 
+{
+    int a = 5;
+    int b = 3;
+
+    int result = power(a,b);
+    return result;
 }
