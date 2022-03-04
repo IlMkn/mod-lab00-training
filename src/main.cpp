f24245d06adc35190a326629c37025724d2e971c
IlMkn <91212388+IlMkn@users.noreply.github.com>
2022-03-04 15:55:40 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,9 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
-#include <iostream>
-using namespace std;
 
-int main() 
-{
+int main(){
     int a = 2;
     int b = 4;
-
     int result = power(a, b);
     return result;
 }
