5dc8719df65b3985af87fd11bba5a9245ca821b6
IlMkn <91212388+IlMkn@users.noreply.github.com>
2022-03-04 15:55:17 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,16 +1,13 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
 
-int64_t power(int64_t x, uint16_t n) 
-{
+int64_t power(int64_t x, uint16_t n){
     int result = 0;
     if (n == 0)
         result = 1;
-    else
-    {
+    else{
         result = x;
-        for (int i = 1; i < n; i++)
-        {
+        for (int i = 0; i < n; i++){
             result = result * x;
         }
     }
